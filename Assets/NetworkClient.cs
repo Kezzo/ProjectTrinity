a85742a1fc60e0c28a4dd5b5f81ee71bd1996470
Frank Roeder <3roeder@informatik.uni-hamburg.de>
2018-09-20 01:24:44 +0200

    Use one Socket for Communication

--- Assets/NetworkClient.cs
+++ Assets/NetworkClient.cs
@@ -11,36 +11,36 @@ public class NetworkClient : MonoBehaviour {
   // Use this for initialization
   void Start () {
     try {
-      StartListen ();
-      StartCoroutine (StartSend ());
+      Socket s = new Socket (AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+      IPAddress broadcast = IPAddress.Parse ("127.0.0.1");
+      IPEndPoint srcIpEndPoint = new IPEndPoint (broadcast, 1337);
+      s.Bind (srcIpEndPoint);
+      IPEndPoint ep = new IPEndPoint (IPAddress.Parse ("127.0.0.1"), 2448);
+      s.SendTo (Encoding.ASCII.GetBytes ("Start"), ep);
+
+      StartCoroutine (ListenForMessages (s));
+      StartCoroutine (SendMessages (s));
+
     } catch (Exception e) {
       Debug.Log (e.ToString ());
     }
   }
 
-  private void StartListen () {
+  private IEnumerator ListenForMessages (Socket s) {
     Debug.Log ("Waiting for broadcast");
-    Task.Run (async () => {
-      using (var udpClient = new UdpClient (1337)) {
-        while (true) {
-          //IPEndPoint object will allow us to read datagrams sent from any source.
-          var receivedResults = await udpClient.ReceiveAsync ();
-          var buffer = receivedResults.Buffer;
-          Debug.LogFormat ("Received broadcast from : {0}\n",
-            Encoding.ASCII.GetString (buffer, 0, buffer.Length));
-        }
-      }
-    });
+    while (true) {
+      byte[] b = new byte[100];
+      int k = s.Receive (b);
+      string szReceived = Encoding.ASCII.GetString (b, 0, k);
+      Debug.LogFormat (szReceived);
+      yield return new WaitForSeconds (1f);
+    }
   }
-  private IEnumerator StartSend () {
-
-    Socket s = new Socket (AddressFamily.InterNetwork, SocketType.Dgram,
-      ProtocolType.Udp);
-
+  private IEnumerator SendMessages (Socket s) {
     IPAddress broadcast = IPAddress.Parse ("127.0.0.1");
-
+    IPEndPoint ep = new IPEndPoint (broadcast, 2448); // endpoint where server is listening
+    //client.Connect(ep);
     byte[] sendbuf = Encoding.ASCII.GetBytes ("HALLO");
-    IPEndPoint ep = new IPEndPoint (broadcast, 2448);
     while (true) {
       s.SendTo (sendbuf, ep);
       Debug.LogFormat ("Message sent to the broadcast address");
