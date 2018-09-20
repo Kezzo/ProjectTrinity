706fcc8a982fdf5743f7c5ca5fee1ab5bd851029
Jon Jelinski <jon_jelinski@t-online.de>
2018-09-21 00:16:20 +0300

    Remove PubNub and old test code

--- Assets/Plugins/PubNubUnity/Assets/Helpers/CommonText.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace PubNubAPI
-{
-    internal static class CommonText
-    {
-        internal static readonly string DuplicateChannelsOrChannelGroups = "Duplicate Channels or Channel Groups";
-    }
-}
