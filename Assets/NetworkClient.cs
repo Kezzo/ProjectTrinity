0c57e5600fa62aab562748d66818a23999d4b025
Jon Jelinski <jon_jelinski@t-online.de>
2018-09-21 22:31:54 +0300

    Implement network time synch service

--- Assets/NetworkClient.cs
+++ Assets/NetworkClient.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
 using UnityEngine;
 
 public class NetworkClient : MonoBehaviour {
   // Use this for initialization
   void Start () {
+
+    var networkTimeService = new NetworkTimeService();
+    networkTimeService.Synch("pool.ntp.org", () => {
+      
+    });
+
     try {
       Socket s = new Socket (AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
       IPAddress broadcast = IPAddress.Parse ("127.0.0.1");
