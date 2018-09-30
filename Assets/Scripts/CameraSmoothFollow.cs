462d69fe95c4c7dedca0ca8d869d8aa24d5a213a
Mazinkam_Android <ali.abdulkarim90@gmail.com>
2018-09-30 12:43:45 +0300

    Added UniRx Added Zenject Added prototype for player controls Added sample scene

---
+++ Assets/Scripts/CameraSmoothFollow.cs
@@ -0,0 +1,56 @@
+﻿using UniRx;
+using UnityEngine;
+
+ public class CameraSmoothFollow : MonoBehaviour
+ {
+     // The target we are following
+     public Transform Target;
+     // The distance in the x-z plane to the target
+     public float Distance = 10.0f;
+     // the height we want the camera to be above the target
+     public float Height = 5.0f;
+     // How much we 
+     public float HeightDamping = 2.0f;
+     public float RotationDamping = 3.0f;
+
+	 private Transform cameraTransform;
+
+	 private void Awake()
+	 {
+		 cameraTransform = GetComponent<Transform>();
+		 Observable.EveryLateUpdate().Subscribe(_ => SmoothFollowTarget()).AddTo(this);
+	 }
+
+	 private void  SmoothFollowTarget ()
+     {
+         // Early out if we don't have a target
+         if (!Target)
+             return;
+     
+         // Calculate the current rotation angles
+         var wantedRotationAngle = Target.eulerAngles.y;
+         var wantedHeight = Target.position.y + Height;
+         var currentRotationAngle = cameraTransform.eulerAngles.y;
+         var currentHeight = cameraTransform.position.y;
+     
+         // Damp the rotation around the y-axis
+         currentRotationAngle = Mathf.LerpAngle (currentRotationAngle, wantedRotationAngle, RotationDamping * Time.deltaTime);
+ 
+         // Damp the height
+         currentHeight = Mathf.Lerp (currentHeight, wantedHeight, HeightDamping * Time.deltaTime);
+ 
+         // Convert the angle into a rotation
+         var currentRotation = Quaternion.Euler (0, currentRotationAngle, 0);
+     
+         // Set the position of the camera on the x-z plane to:
+         // distance meters behind the target
+	     cameraTransform.position = Target.position;
+	     cameraTransform.position -= currentRotation * Vector3.forward * Distance;
+ 
+         // Set the height of the camera
+	     cameraTransform.position = new Vector3(cameraTransform.position.x, currentHeight, cameraTransform.position.z);
+     
+         // Always look at the target
+	     cameraTransform.LookAt (Target);
+     }
+ }
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using UniRx;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	[Header("Joysticks")] 
+	public Joystick MovementJoystick;
+	public Joystick AimingJoystick;
+	
+	[Header("Player Variables")] 
+	public int MoveSpeed;
+	public Transform PlayerTransform;
+
+	private void Awake()
+	{
+		Observable.EveryUpdate()
+			.Subscribe(_ =>
+			           {
+				           UpdateMovementJoystick();
+				           UpdateAimingJoystick();
+			           }).AddTo(this);
+	}
+
+	private void UpdateMovementJoystick()
+	{
+		var moveVector = PlayerTransform.right * MovementJoystick.Horizontal + PlayerTransform.forward * MovementJoystick.Vertical;
+
+		if (moveVector == Vector3.zero)
+			return;
+
+		PlayerTransform.Translate(moveVector * MoveSpeed * Time.deltaTime, Space.World);
+	}
+
+	private void UpdateAimingJoystick()
+	{
+		var aimVector = Vector3.right * AimingJoystick.Horizontal + Vector3.forward * AimingJoystick.Vertical;
+
+		if (aimVector == Vector3.zero)
+			return;
+
+		PlayerTransform.rotation = Quaternion.LookRotation(aimVector);
+	}
+}
