1086cf99bef13b2eac05b0d051e46c179cb60d9d
Jon Jelinski <jon_jelinski@t-online.de>
2018-09-30 23:06:44 +0300

    Add test input

--- Assets/Scripts/Root.cs
+++ Assets/Scripts/Root.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class Root : MonoBehaviour 
 {
     private MatchStateMachine matchStateMachine;
-    
 
     private void Start()
     {
@@ -17,5 +16,27 @@ public class Root : MonoBehaviour
     {
         matchStateMachine.OnFixedUpdateTick();
         DIContainer.AckedMessageHelper.OnFixedUpdateTick();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            Debug.Log("W");
+            matchStateMachine.MatchInputProvider.AddXTranslation(1f);
+        } 
+        else if(Input.GetKey(KeyCode.S))
+        {
+            matchStateMachine.MatchInputProvider.AddXTranslation(-1f);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            matchStateMachine.MatchInputProvider.AddYTranslation(1f);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            matchStateMachine.MatchInputProvider.AddYTranslation(-1f);
+        }
     }
 }
